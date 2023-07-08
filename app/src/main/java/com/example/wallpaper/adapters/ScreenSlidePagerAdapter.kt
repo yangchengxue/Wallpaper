0b2825bf2159495d0c0760b17b61737b1bcd5a4c
ycx <553566463@qq.com>
2023-07-08 16:33:16 +0800

    feat:[Wallpaper] 兼容22，调整代码

--- app/src/main/java/com/example/wallpaper/adapters/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/com/example/wallpaper/adapters/ScreenSlidePagerAdapter.kt
@@ -4,12 +4,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.wallpaper.fragments.WallpapersFragment
+import com.example.wallpaper.model.BaseData.VIEW_PAGER_NUM_PAGES
 
 class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
 
-    private val NUM_PAGES = 5
-
-    override fun getItemCount(): Int = NUM_PAGES
+    override fun getItemCount(): Int = VIEW_PAGER_NUM_PAGES
 
     override fun createFragment(position: Int): Fragment = WallpapersFragment.newInstance()
 }
--- app/src/main/java/com/example/wallpaper/model/BaseData.kt
+++ app/src/main/java/com/example/wallpaper/model/BaseData.kt
@@ -1,5 +1,7 @@
 package com.example.wallpaper.model
 
 object BaseData {
-    val WALLPAPER_STYLE_LIST = ArrayList(listOf("精选", "暗黑", "唯美", "情侣", "酷炫"))
+    val WALLPAPER_STYLE_LIST = ArrayList(listOf("精选", "暗黑", "唯美", "动漫", "酷炫", "卡通"))
+
+    val VIEW_PAGER_NUM_PAGES = WALLPAPER_STYLE_LIST.size
 }
